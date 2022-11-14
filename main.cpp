fc6cbebd4d342c330c0268fb2f97c80980c319bc
Fitra Fep <109885309+fepfitra@users.noreply.github.com>
2022-11-14 00:32:29 +0000

    divide some jobs

--- main.cpp
+++ main.cpp
@@ -2,7 +2,25 @@
 
 using namespace std;
 
+char board[5][5]; //pakai papan 3x3, nilai hanya X, O, dan _
+bool isPlayerx = true;
+struct {
+  string name;
+  int score;
+} playerx, playero;
+
+void init(); //Todo mengisi data dan menampilkan papan kosong
+bool isAvailable(int row, int column); //mengecek apakah bisa diisi atau tidak
+bool isInputValid(int number); //mengecek apakah inputnya valdi
+
+void clearScreen(); //menghapus layar
+
+bool isEnd(); //mengecek apakah tabel terisi semua
+
+void showBoard(); //menampilkan semua data
+
 int main() {
-  cout << "Hello World!\n";
-  //test
+  init();
+  showBoard();
+  return 0;
 }
